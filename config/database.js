acf614dc34e0ec45ae2999f4eb81c999cdd55bec
Ngo Van Hau <ngovanhau2109@gmail.com>
2026-02-06 08:26:41 +0700

    db config

--- config/database.js
+++ config/database.js
@@ -9,6 +9,18 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
   ssl: process.env.PGSSLMODE === 'require',
   logging: false,
+  dialectOptions: {
+    keepAlive: true, 
+  },
+  pool: {
+    max: 5,        // Số kết nối tối đa
+    min: 0,
+    acquire: 30000, // Thời gian tối đa để thử kết nối (ms)
+    idle: 10000     // Nếu kết nối rảnh quá 10s thì ngắt đi, đừng giữ
+  },
+  define: {
+    timestamps: true
+  },
 });
 
 module.exports = { sequelize };
